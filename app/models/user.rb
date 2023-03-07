b29cd8bbcb9ef530eb0d894d6bb8f99e51e333bf
hiroki <you@example.com>
2023-03-07 04:14:19 +0000

    Application task-5 finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class User < ApplicationRecord
    validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
    validates :introduction,length: {maximum:50}
 
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 
   def follow(user_id)
     relationships.create(followed_id: user_id)
@@ -32,7 +35,18 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
-  end
+
+   def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+   end
 end
